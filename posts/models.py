fba80aa0f9b2a6edca04ec761a0ee800dc850d51
2sseul2 <2sseul2yoo@gmail.com>
2022-06-23 14:46:00 +0900

    검색 결과 조건 2개 지정 & 검색/결과/세부 html 수정

--- posts/models.py
+++ posts/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    summary = models.CharField(max_length=200, default="")
     date = models.DateTimeField(auto_now_add=True)
     
--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views as posts_views
 
+app_name = "posts"   
+
 urlpatterns = [  
-    #-- 검색 테스트 위해 임시로 만든 검색 URL --# 
-    path('search/', posts_views.search, name='search'),  #posts/search: 검색 폼 -> views.search
-    
-    path('', posts_views.postList, name='postList'), # posts/: 검색 결과 목록 -> views.postList
+
+    path('', posts_views.home, name='home'),  #posts/: 홈 
+    path('search/', posts_views.postList, name='postList'), # posts/search/keyword: 검색 결과 목록 -> views.postList
     path('<int:postId>/', posts_views.detail, name='detail'), # posts/{postId}: 상세 페이지 -> views.detail
 ]
+
