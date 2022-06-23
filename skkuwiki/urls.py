ca20e1ffcf080dbca196b7302b1974b27f355831
2sseul2 <2sseul2yoo@gmail.com>
2022-06-23 14:42:47 +0900

    홈 링크 추가

--- skkuwiki/urls.py
+++ skkuwiki/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
+    path('', include('posts.urls')),
     path('posts/', include('posts.urls')),
     path('account/', include('account.urls')),
 ]
